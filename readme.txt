ce7a9f7bc88eadf8e08219d3ec26385ce7a02a6f
unknown <kartikeyad@synapseco.com>
2018-06-16 10:41:35 +0530

    Initial upload

---
+++ readme.txt
@@ -0,0 +1,16 @@
+This Opencart extension allow customers to post topic and replies on forum. A topic can have multiple replies.
+
+Main advantages of this extension
+
+Easy to install, just need to use inbuilt feature of OpenCart - OCMOD (Extension Installer).
+Helping hand for Customers - Customers can get reviews on forum related to any product or services.
+Helps build brand popularity
+
+Features :
+Logged in Customer can post topic on the website.
+Admin have to approve the topics to visible on the forum list.
+Admin also can create topics.
+Customers can reply to any topic.
+Email notification to admin to when any customer posts a new topic.
+Reply counter
+[*] View Counter by topic.
---
+++ upload/catalog/language/english/module/forum.php
@@ -0,0 +1,50 @@
+<?php
+// Heading 
+$_['heading_title']  = 'Discussion Forum';
+$_['text_forum']     = 'Discussion Forums';
+$_['topic']          = 'Start Discussion';
+$_['post_topic']     = 'Post New Topic';
+$_['post_reply']     = 'Post Reply';
+
+// Text
+$_['text_success']       = 'Your topic has been submitted. After approval of the administrator will be published';
+$_['text_success_reply'] = 'Your reply has been submitted.';
+$_['text_default']       = 'Default';
+$_['text_image_manager'] = 'Image Manager';
+$_['text_browse']        = 'Browse';
+$_['text_clear']         = 'Clear'; 
+$_['text_error']         = 'File Not Found';
+
+
+// Column
+$_['column_name']        = 'Forum Name';
+$_['column_status']      = 'Status';
+$_['column_action']      = 'Action';
+
+// Entry
+$_['entry_name']         = 'Name';
+$_['entry_email']        = 'Email';
+$_['entry_avatar']       = 'Your Avatar';
+$_['entry_topic_title']  = 'Topic Title';
+$_['entry_topic_message']= 'Topic Message';
+$_['entry_topic_reply']  = 'Reply';
+
+//Button
+$_['button_add_forum']       = 'Add Forum';
+
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify forums!';
+$_['error_name']         = 'Forum Name must be between 3 and 64 characters!';
+$_['error_username']     = 'Username must be between 3 and 64 characters!';
+$_['error_email']        = 'Email must be between 3 and 64 characters!';
+$_['error_avatar']       = 'Please Upload your avatar!';
+$_['error_name']         = 'Title must be between 3 and 64 characters!';
+$_['error_description']  = 'Description must be between 3 and 64 characters!';
+$_['error_reply']  		 = 'Reply must be between 3 and 64 characters!';
+$_['error_title']        = 'Forum Title must be between 2 and 64 characters!';
+$_['error_default']      = 'Warning: This layout cannot be deleted as it is currently assigned as the default store layout!';
+$_['error_product']      = 'Warning: This layout cannot be deleted as it is currently assigned to %s products!';
+$_['error_category']     = 'Warning: This layout cannot be deleted as it is currently assigned to %s categories!';
+$_['error_information']  = 'Warning: This layout cannot be deleted as it is currently assigned to %s information pages!';
+?>
